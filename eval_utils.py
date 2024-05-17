619ecfd5f0e0e2b3cec392e0c01092dd14b5a144
Jonathan Burkow <burkowjo@msu.edu>
2024-05-17 18:41:32 -0400

    fixed float returns in MetricsConfMatrix; added nms functions

--- eval_utils.py
+++ eval_utils.py
@@ -1,7 +1,7 @@
 '''
 Filename: eval_utils.py
 Author(s): Jonathan Burkow, burkowjo@msu.edu, Michigan State University
-Last Updated: 03/31/2022
+Last Updated: 09/06/2022
 Description: Various utility functions used for evaluating performance of the model on detection.
 '''
 
@@ -11,7 +11,9 @@ from typing import List, Optional, Tuple
 import cv2
 import numpy as np
 import pandas as pd
+import torch
 from scipy.integrate import simps
+from torchvision.ops import nms
 
 
 class MetricsConfMatrix:
@@ -44,27 +46,27 @@ class MetricsConfMatrix:
 
     def precision(self) -> float:
         """Calculate precision."""
-        return 0 if float(self.true_pos + self.false_pos) == 0 else \
+        return 0. if float(self.true_pos + self.false_pos) == 0 else \
             self.true_pos / float(self.true_pos + self.false_pos)
 
     def recall(self) -> float:
         """Calculate recall."""
-        return 0 if float(self.true_pos + self.false_neg) == 0 else \
+        return 0. if float(self.true_pos + self.false_neg) == 0 else \
             self.true_pos / float(self.true_pos + self.false_neg)
 
     def f1_score(self) -> float:
         """Calculate F1 score."""
-        return 0 if (self.precision() + self.recall()) == 0 else \
+        return 0. if (self.precision() + self.recall()) == 0 else \
             2 * (self.precision() * self.recall()) / (self.precision() + self.recall())
 
     def f2_score(self) -> float:
         """Calculate F2 score."""
-        return 0 if (2**2 * self.precision() + self.recall()) == 0 else \
+        return 0. if (2**2 * self.precision() + self.recall()) == 0 else \
             (1 + 2**2)*(self.precision() * self.recall()) / (2**2*self.precision() + self.recall())
 
     def fpr(self) -> float:
         """Calculate false positive rate."""
-        return 0 if float(self.false_pos + self.true_neg) == 0 else \
+        return 0. if float(self.false_pos + self.true_neg) == 0 else \
             self.false_pos / float(self.false_pos + self.true_neg)
 
     def cohens_kappa(self) -> float:
@@ -81,7 +83,7 @@ class MetricsConfMatrix:
 
     def free_kappa(self) -> float:
         """Calculate Free-Response Kappa (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5395923/)."""
-        return 0 if float(self.false_pos + self.false_neg + 2 * self.true_pos) == 0 else \
+        return 0. if float(self.false_pos + self.false_neg + 2 * self.true_pos) == 0 else \
             2 * self.true_pos / float(self.false_pos + self.false_neg + 2 * self.true_pos)
 
 
@@ -304,7 +306,8 @@ def calc_auc(afroc_df: pd.DataFrame) -> float:
     auc = simps(afroc_df['LLF'], afroc_df['Threshold'])
 
     # Backup AUC calculation if Simpson's Rule fails
-    if auc < 0.0 and auc > 1.0:
+    # if auc < 0.0 and auc > 1.0:
+    if 0.0 > auc > 1.0:
         auc = 0
         for i in range(len(afroc_df) - 1):
             dx = afroc_df['Threshold'].iloc[i+1] - afroc_df['Threshold'].iloc[i]
@@ -479,3 +482,74 @@ def _compute_ap(
     i = np.where(mrec[1:] != mrec[:-1])[0]
 
     return np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+
+
+def non_max_suppression(
+        data_df: pd.DataFrame,
+        df_names: List[str] = None,
+        iou_thresh=0.55
+    ) -> Tuple[List[int], List[int], List[int], List[int], List[float], List[bool]]:
+    """
+    Applies non-maximum suppression (NMS) to the provided data_df and outputs all remaining bounding
+    boxes and the kept indices.
+
+    Parameters
+    ----------
+    data_df    : DataFrame of current patient's bounding box predictions
+    df_names   : column headers for the DataFrame
+    iou_thresh : IOU threshold for discarded boxes during non-max suppression
+
+    Returns
+    -------
+    x1_list    : list of all top left x-values of NMS-accepted bounding boxes
+    y1_list    : list of all top left y-values of NMS-accepted bounding boxes
+    x2_list    : list of all bottom left x-values of NMS-accepted bounding boxes
+    y2_list    : list of all bottom left y-values of NMS-accepted bounding boxes
+    score_list : list of all corresponding scores of bounding boxes
+    keep       : boolean list of accepted bounding boxes from NMS
+    """
+    # Convert to float tensors for PyTorch NMS usage
+    boxes = torch.from_numpy(data_df[['x1', 'y1', 'x2', 'y2']].values.astype(np.float64))
+    scores = torch.from_numpy(data_df[df_names[-1]].values.astype(np.float64))
+
+    # Run NMS and collect results
+    keep = nms(boxes, scores, iou_threshold=iou_thresh)
+
+    x1_list = boxes[keep, 0].int().tolist()
+    y1_list = boxes[keep, 1].int().tolist()
+    x2_list = boxes[keep, 2].int().tolist()
+    y2_list = boxes[keep, 3].int().tolist()
+    score_list = scores[keep].tolist()
+
+    return x1_list, y1_list, x2_list, y2_list, score_list, keep
+
+
+def apply_nms(
+        data_df: pd.DataFrame,
+        df_names: List[str] = None,
+        iou_thresh: float = 0.55
+    ) -> pd.DataFrame:
+    # For each image: (1) gather all detections across ensemble members + (2) perform NMS on that set of detections
+    img_paths = []
+    x1_list, y1_list, x2_list, y2_list = [], [], [], []
+    all_scores = []
+    for img_path in list(set(data_df[df_names[0]])):
+        # Subset data frame for all detections on image
+        df = data_df[data_df[df_names[0]] == img_path]
+
+        # Remove NAs... if nothing remains (no detections from any model), continue
+        df = df.dropna(subset=df_names[1:])
+
+        if df.shape[0] == 0:
+            continue
+
+        patient_x1s, patient_y1s, patient_x2s, patient_y2s, score_list, keep = non_max_suppression(df, df_names, iou_thresh)
+
+        img_paths.extend([img_path]*keep.shape[0])
+        x1_list.extend(patient_x1s)
+        y1_list.extend(patient_y1s)
+        x2_list.extend(patient_x2s)
+        y2_list.extend(patient_y2s)
+        all_scores.extend(score_list)
+
+    return pd.DataFrame({df_names[0]: img_paths, 'x1': x1_list, 'y1': y1_list, 'x2': x2_list, 'y2': y2_list, df_names[-1]: all_scores})
