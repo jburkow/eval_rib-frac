5699feca3965f36617bd5486a90dfca56bcac7bf
Jonathan Burkow <burkowjo@msu.edu>
2024-05-17 18:39:45 -0400

    moved nms to eval_utils; fixed args import

--- ensemble.py
+++ ensemble.py
@@ -10,92 +10,17 @@ Description: Takes in a list of model prediction CSVs and creates a new ensemble
 import argparse
 import os
 import time
-from typing import List, Tuple
 
-import numpy as np
 import pandas as pd
-import torch
-from torchvision.ops import nms
-
-import avalanche_predictions
+from avalanche_predictions import AVALANCHE_CONFIGS, get_avalanche_df
+from eval_utils import apply_nms
 from general_utils import convert_seconds
-
-
-def non_max_suppression(
-        data_df: pd.DataFrame,
-        df_names: List[str] = None,
-        iou_thresh=0.55
-    ) -> Tuple[List[int], List[int], List[int], List[int], List[float], List[bool]]:
-    """
-    Applies non-maximum suppression (NMS) to the provided data_df and outputs all remaining bounding
-    boxes and the kept indices.
-
-    Parameters
-    ----------
-    data_df    : DataFrame of current patient's bounding box predictions
-    df_names   : column headers for the DataFrame
-    iou_thresh : IOU threshold for discarded boxes during non-max suppression
-
-    Returns
-    -------
-    x1_list    : list of all top left x-values of NMS-accepted bounding boxes
-    y1_list    : list of all top left y-values of NMS-accepted bounding boxes
-    x2_list    : list of all bottom left x-values of NMS-accepted bounding boxes
-    y2_list    : list of all bottom left y-values of NMS-accepted bounding boxes
-    score_list : list of all corresponding scores of bounding boxes
-    keep       : boolean list of accepted bounding boxes from NMS
-    """
-    # Convert to float tensors for PyTorch NMS usage
-    boxes = torch.from_numpy(data_df[['x1', 'y1', 'x2', 'y2']].values.astype(np.float64))
-    scores = torch.from_numpy(data_df[df_names[-1]].values.astype(np.float64))
-
-    # Run NMS and collect results
-    keep = nms(boxes, scores, iou_threshold=iou_thresh)
-
-    x1_list = boxes[keep, 0].int().tolist()
-    y1_list = boxes[keep, 1].int().tolist()
-    x2_list = boxes[keep, 2].int().tolist()
-    y2_list = boxes[keep, 3].int().tolist()
-    score_list = scores[keep].tolist()
-
-    return x1_list, y1_list, x2_list, y2_list, score_list, keep
-
-
-def apply_nms(
-        data_df: pd.DataFrame,
-        df_names: List[str] = None,
-        iou_thresh: float = 0.55
-    ) -> pd.DataFrame:
-    # For each image: (1) gather all detections across ensemble members + (2) perform NMS on that set of detections
-    img_paths = []
-    x1_list, y1_list, x2_list, y2_list = [], [], [], []
-    all_scores = []
-    for img_path in list(set(data_df[df_names[0]])):
-        # Subset data frame for all detections on image
-        df = data_df[data_df[df_names[0]] == img_path]
-
-        # Remove NAs... if nothing remains (no detections from any model), continue
-        df = df.dropna(subset=df_names[1:])
-
-        if df.shape[0] == 0:
-            continue
-
-        patient_x1s, patient_y1s, patient_x2s, patient_y2s, score_list, keep = non_max_suppression(df, df_names, iou_thresh)
-
-        img_paths.extend([img_path]*keep.shape[0])
-        x1_list.extend(patient_x1s)
-        y1_list.extend(patient_y1s)
-        x2_list.extend(patient_x2s)
-        y2_list.extend(patient_y2s)
-        all_scores.extend(score_list)
-
-    return pd.DataFrame({df_names[0]: img_paths, 'x1': x1_list, 'y1': y1_list, 'x2': x2_list, 'y2': y2_list, df_names[-1]: all_scores})
 
 
 def make_ensemble(
         model_pred_list: str,
         images_path: str,
-        df_names: List[str] = None,
+        df_names: list[str] = None,
         nms_iou_thresh: float = 0.55
     ) -> pd.DataFrame:
     """
@@ -138,12 +63,12 @@ def make_ensemble(
 
 
 def main(args) -> None:
-    """Main Function""" 
+    """Main Function"""
     ens_df = make_ensemble(args.preds, args.image_path)
     if args.avalanche:
-        base_value = avalanche_predictions.AVALANCHE_CONFIGS[args.avalanche]['base_val']
-        rate = avalanche_predictions.AVALANCHE_CONFIGS[args.avalanche]['rate']
-        ens_df = avalanche_predictions.get_avalanche_df(ens_df, 'avalanche' if 'gamma' in args.avalanche else args.avalanche, base_value, rate, add_nms=True, df_cols=['img_path', 'x1', 'y1', 'x2', 'y2', 'score'])
+        base_value = AVALANCHE_CONFIGS[args.avalanche]['base_val']
+        rate = AVALANCHE_CONFIGS[args.avalanche]['rate']
+        ens_df = get_avalanche_df(ens_df, 'avalanche' if 'gamma' in args.avalanche else args.avalanche, base_value, rate, add_nms=True, df_cols=['img_path', 'x1', 'y1', 'x2', 'y2', 'score'])
     ens_df.to_csv(os.path.join(args.save_dir, f"{args.ensemble_name}.csv"), index=False, header=False)
 
 
