a050984664628cf9a1d53b1e9fde4c56cc719fbd
Jonathan Burkow <burkowjo@msu.edu>
2024-05-21 01:34:00 -0400

    import fixes; minor code cleanup

--- avalanche_predictions.py
+++ avalanche_predictions.py
@@ -1,8 +1,7 @@
 '''
 Filename: avalanche_predictions.py
-Author: Jonathan Burkow, burkowjo@msu.edu
-        Michigan State University
-Last Updated: 03/21/2022
+Author(s): Jonathan Burkow, burkowjo@msu.edu, Michigan State University
+Last Updated: 05/15/2024
 Description: Takes a provided model predictions CSV file and applies an avalanche decision scheme
     to it. A new CSV file is created with the predictions that are kept after the scheme is applied.
 '''
@@ -10,43 +9,61 @@ Description: Takes a provided model predictions CSV file and applies an avalanch
 import argparse
 import os
 import time
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
-
 from eval_utils import apply_nms
-from general_utils import convert_seconds
 
 AVALANCHE_CONFIGS = {
-    "standard" : {
-        "model_conf" : 0.50,
-        "base_val" : 0.50,
-        "rate" : 0.00
+    "standard": {
+        "model_conf": 0.50,
+        "base_val": 0.50,
+        "rate": 0.00
     },
-    "posterior" : {
-        "model_conf" : 0.00,
-        "base_val" : 0.50,
-        "rate" : 0.00
+    "posterior": {
+        "model_conf": 0.00,
+        "base_val": 0.50,
+        "rate": 0.00
     },
-    "conservative" : {
-        "model_conf" : 0.00,
-        "base_val" : 0.45,
-        "rate" : 0.00
+    "conservative": {
+        "model_conf": 0.00,
+        "base_val": 0.45,
+        "rate": 0.00
     },
-    "gamma15" : {
-        "model_conf" : 0.00,
-        "base_val" : 0.40,
-        "rate" : 0.15
+    "gamma15": {
+        "model_conf": 0.00,
+        "base_val": 0.40,
+        "rate": 0.15
     },
-    "gamma20" : {
-        "model_conf" : 0.00,
-        "base_val" : 0.60,
-        "rate" : 0.20
+    "gamma20": {
+        "model_conf": 0.00,
+        "base_val": 0.60,
+        "rate": 0.20
     }
 }
 
 
-def apply_scheme(patient_df: pd.DataFrame, scheme: str, base_value: float, rate: float, df_cols: list[str] = None) -> pd.DataFrame:
+def parse_cmd_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--csv_preds', type=str, required=True, help='Filename to CSV containing model predictions.')
+    parser.add_argument('--method', choices=['standard', 'posterior', 'conservative', 'avalanche'], help='Which type of avalanche scheme to use.')
+    parser.add_argument('--base_val', type=float, default=0.50, help='Starting model confidence to begin the avalanche scheme.')
+    parser.add_argument('--rate', type=float, default=0.05, help='The percentage that each successive avalanche scheme step drops by.')
+    parser.add_argument('--save_dir', default='./out_files', help='Default folder to save files to.')
+    parser.add_argument('--filename', help='Name to save new CSV files as.')
+    parser.add_argument('--no_save', action='store_true', help='If true, does not save resulting DataFrame to a CSV.')
+
+    return parser.parse_args()
+
+
+def apply_scheme(
+        patient_df: pd.DataFrame,
+        scheme: str,
+        base_value: float,
+        rate: float,
+        df_cols: list[str] = None) -> pd.DataFrame:
     """
     Applies one of the avalanche schemes to the patient_df and outputs a DataFrame of all
     predictions accepted for the current patient.
@@ -69,13 +86,13 @@ def apply_scheme(patient_df: pd.DataFrame, scheme: str, base_value: float, rate:
         # avalanche_percentages = [1, 0.7643979057591623, 0.8424657534246576, 0.7723577235772358, 0.7842105263157895]  # 20210420 data
         avalanche_percentages = [1, 0.7364864864864865, 0.8134556574923547, 0.7669172932330827, 0.7696078431372549]  # 20210902 data
     elif scheme == "posterior":
-        # avalanche_percentages = [1, 1-0.7643979057591623, 1-0.8424657534246576, 1-0.7723577235772358, 1-0.7842105263157895]  # 20210420 data
-        avalanche_percentages = [1, 1-0.7364864864864865, 1-0.8134556574923547, 1-0.7669172932330827, 1-0.7696078431372549]  # 20210902 data
+        # avalanche_percentages = [1, 1 - 0.7643979057591623, 1 - 0.8424657534246576, 1 - 0.7723577235772358, 1 - 0.7842105263157895]  # 20210420 data
+        avalanche_percentages = [1, 1 - 0.7364864864864865, 1 - 0.8134556574923547, 1 - 0.7669172932330827, 1 - 0.7696078431372549]  # 20210902 data
     else:
         avalanche_percentages = []
 
     # Create the test thresholds list to use when determining what model predictions are kept
-    thresholds = [base_value * val for val in [np.prod(np.array(avalanche_percentages[:k])) for k in range(1, len(avalanche_percentages)+1)]]
+    thresholds = [base_value * val for val in [np.prod(np.array(avalanche_percentages[:k])) for k in range(1, len(avalanche_percentages) + 1)]]
 
     start_df = patient_df[patient_df[df_cols[-1]] >= thresholds[0]]
     num_model_boxes = len(start_df)
@@ -85,7 +102,7 @@ def apply_scheme(patient_df: pd.DataFrame, scheme: str, base_value: float, rate:
     while num_model_boxes > prior_num_model_boxes:
         prior_num_model_boxes = num_model_boxes
         # Set threshold level based on number of already accepted fractures
-        test_thresh_ind = min(len(thresholds)-1, num_model_boxes)
+        test_thresh_ind = min(len(thresholds) - 1, num_model_boxes)
         thresh = thresholds[test_thresh_ind]
         step_df = patient_df[patient_df[df_cols[-1]] >= thresh]
         num_model_boxes = len(step_df)
@@ -96,7 +113,13 @@ def apply_scheme(patient_df: pd.DataFrame, scheme: str, base_value: float, rate:
     return start_df if thresh is None else step_df
 
 
-def get_avalanche_df(data_df: pd.DataFrame, scheme: str, base_value: float, rate: float, add_nms: bool = False, df_cols: list[str] = None) -> pd.DataFrame:
+def get_avalanche_df(
+        data_df: pd.DataFrame,
+        scheme: str,
+        base_value: float,
+        rate: float,
+        add_nms: bool = False,
+        df_cols: list[str] = None) -> pd.DataFrame:
     """
     Apply an avalanche scheme to the model predictions DataFrame and output the final DataFrame of
     model predictions.
@@ -122,15 +145,18 @@ def get_avalanche_df(data_df: pd.DataFrame, scheme: str, base_value: float, rate
             scheme_df = apply_scheme(df_group, scheme, base_value, rate, df_cols)
 
         if len(scheme_df) == 0:
-            out_df = out_df.append({df_cols[0] : group_name, df_cols[1] : pd.NA, df_cols[2] : pd.NA, df_cols[3] : pd.NA, df_cols[4] : pd.NA, df_cols[5] : pd.NA}, ignore_index=True)
+            new_df = pd.DataFrame({df_cols[0]: [group_name], df_cols[1]: [pd.NA], df_cols[2]: [pd.NA], df_cols[3]: [pd.NA], df_cols[4]: [pd.NA], df_cols[5]: [pd.NA]})
+            out_df = pd.concat([out_df, new_df], ignore_index=True)
         else:
-            out_df = out_df.append(scheme_df, ignore_index=True)
+            out_df = pd.concat([out_df, scheme_df], ignore_index=True)
 
     return out_df if not add_nms or scheme == "standard" else apply_nms(out_df, df_cols, 0.45)
 
 
-def main(pargs):
+def main() -> None:
     """Main Function"""
+    pargs = parse_cmd_args()
+
     df_cols = ['ID', 'x1', 'y1', 'x2', 'y2', 'Prob']
     data_df = pd.read_csv(pargs.csv_preds, names=df_cols,
                           dtype={'x1': pd.Int64Dtype(), 'y1': pd.Int64Dtype(), 'x2': pd.Int64Dtype(), 'y2': pd.Int64Dtype()})
@@ -144,34 +170,9 @@ def main(pargs):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--csv_preds', type=str, required=True,
-                        help='Filename to CSV containing model predictions.')
-
-    parser.add_argument('--method', choices=['standard', 'posterior', 'conservative', 'avalanche'],
-                        help='Which type of avalanche scheme to use.')
-
-    parser.add_argument('--base_val', type=float, default=0.50,
-                        help='Starting model confidence to begin the avalanche scheme.')
-
-    parser.add_argument('--rate', type=float, default=0.05,
-                        help='The percentage that each successive avalanche scheme step drops by.')
-
-    parser.add_argument('--save_dir', default='./out_files',
-                        help='Default folder to save files to.')
-
-    parser.add_argument('--filename',
-                        help='Name to save new CSV files as.')
-
-    parser.add_argument('--no_save', action='store_true',
-                        help='If true, does not save resulting DataFrame to a CSV.')
-
-    args = parser.parse_args()
-
-    print('\nStarting execution...')
+    print(f"\n{'Starting execution: ' + Path(__file__).name:-^80}\n")
     start_time = time.perf_counter()
-    main(args)
+    main()
     elapsed = time.perf_counter() - start_time
-    print('Done!')
-    print(f'Execution finished in {elapsed:.3f} seconds ({convert_seconds(elapsed)}).\n')
+    print(f"\n{'Done!':-^80}")
+    print(f'Execution finished in {elapsed:.3f} seconds ({time.strftime("%-H hr, %-M min, %-S sec", time.gmtime(elapsed))}).\n')
